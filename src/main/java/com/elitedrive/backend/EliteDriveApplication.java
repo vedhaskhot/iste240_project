86a1466727203466d8935d70ca8da6bc267027ed
akhil <ab7962@g.rit.edu>
2026-04-25 16:02:59 +0400

    Added main SpringBoot application class

---
+++ src/main/java/com/elitedrive/backend/EliteDriveApplication.java
@@ -0,0 +1,13 @@
+package com.elitedrive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EliteDriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elitedrive/backend/EliteDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elitedrive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
