8384c7edb7ce88731e41b322336014c9ecb38339
akhil <ab7962@g.rit.edu>
2026-04-25 15:59:57 +0400

    Added Membership Entity

---
+++ src/main/java/com/elitedrive/backend/repositories/MembershipApplicationRepository.java
@@ -0,0 +1,23 @@
+// By: Akhil Biju (752000700)
+package com.elitedrive.backend.repositories;
+
+import com.elitedrive.backend.entities.MembershipApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface MembershipApplicationRepository extends JpaRepository<MembershipApplication, Long> {
+
+    @Query("SELECT m FROM MembershipApplication m WHERE m.status = ?1")
+    List<MembershipApplication> findByStatusCustom(String status);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE MembershipApplication m SET m.status = ?2 WHERE m.id = ?1")
+    void updateStatusById(Long id, String newStatus);
+}
