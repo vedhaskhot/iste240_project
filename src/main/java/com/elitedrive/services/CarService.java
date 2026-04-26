44f8b6ad52c626b0e30de2e57dcf8c4d1305bc34
Eshaan <me3574@rit.edu>
2026-04-26 17:51:13 +0400

    Add missing files from model-car (manual addition after merge state)

---
+++ src/main/java/com/elitedrive/services/CarService.java
@@ -0,0 +1,41 @@
+// Author: Mohammed Saneeh - 421005948
+package com.elitedrive.services;
+
+import com.elitedrive.models.Car;
+import com.elitedrive.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarService {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
+
+    public Optional<Car> getCarById(Long id) {
+        return carRepository.findById(id);
+    }
+
+    public List<Car> searchCars(String keyword) {
+        return carRepository.searchByKeyword(keyword);
+    }
+
+    public Car createCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    public Car updateCar(Long id, Car updatedCar) {
+        updatedCar.setId(id);
+        return carRepository.save(updatedCar);
+    }
+
+    public void deleteCar(Long id) {
+        carRepository.deleteById(id);
+    }
+}
