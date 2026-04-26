16cf62d5608b478d368c6f61a718598b3b1a62d8
Eshaan <me3574@rit.edu>
2026-04-26 18:45:10 +0400

    Edited member repository to add 2 more methods.

--- src/main/java/org/example/elitedrive/repositories/MemberRepository.java
+++ src/main/java/org/example/elitedrive/repositories/MemberRepository.java
@@ -1,5 +1,7 @@
 package org.example.elitedrive.repositories;
 
+//Mohammed Eshaan, UID:418004114//
+
 import org.example.elitedrive.entities.Member;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
